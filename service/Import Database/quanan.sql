8837eec990152378d706683424965c30f80efe31
An Tran <anhanpro2k@gmail.com>
2021-03-31 13:57:00 +0700

    Tao cau truc chung cho project

---
+++ service/Import Database/quanan.sql
@@ -0,0 +1,358 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 31, 2021 at 06:58 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quanan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chitiethoadon`
+--
+
+CREATE TABLE `chitiethoadon` (
+  `MaHD` int(11) NOT NULL,
+  `MaMon` int(11) NOT NULL,
+  `DonGia` bigint(20) NOT NULL,
+  `SoLuong` int(11) NOT NULL,
+  `ThanhTien` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chitietphieunhap`
+--
+
+CREATE TABLE `chitietphieunhap` (
+  `MaPhieuNhap` int(11) NOT NULL,
+  `MaNL` int(11) NOT NULL,
+  `SoLuong` int(11) NOT NULL,
+  `DonGia` int(11) NOT NULL,
+  `ThanhTien` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chucvu`
+--
+
+CREATE TABLE `chucvu` (
+  `MaChucVu` int(11) NOT NULL,
+  `TenChucVu` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dichvu`
+--
+
+CREATE TABLE `dichvu` (
+  `maDV` int(11) NOT NULL,
+  `tenDV` varchar(30) NOT NULL,
+  `phiHoaHong` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hoadon`
+--
+
+CREATE TABLE `hoadon` (
+  `MaHD` int(11) NOT NULL,
+  `MaNV` int(11) NOT NULL,
+  `NgayLapHD` timestamp NOT NULL DEFAULT current_timestamp(),
+  `MaDV` int(11) NOT NULL,
+  `MaDonTrenApp` varchar(30) NOT NULL,
+  `TongTien` bigint(20) NOT NULL,
+  `ChietKhau` bigint(20) NOT NULL,
+  `PhiDichVu` bigint(20) NOT NULL,
+  `TongThu` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loaimon`
+--
+
+CREATE TABLE `loaimon` (
+  `MaLoai` int(11) NOT NULL,
+  `TenLoai` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mon`
+--
+
+CREATE TABLE `mon` (
+  `MaMon` int(11) NOT NULL,
+  `MaLoaiMon` int(11) NOT NULL,
+  `TenMon` varchar(30) NOT NULL,
+  `GiaBan` int(11) NOT NULL,
+  `TinhTrang` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nguyenlieu`
+--
+
+CREATE TABLE `nguyenlieu` (
+  `MaNL` int(11) NOT NULL,
+  `TenNL` varchar(30) NOT NULL,
+  `DonGia` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nhanvien`
+--
+
+CREATE TABLE `nhanvien` (
+  `MaNV` int(11) NOT NULL,
+  `MaCV` int(11) NOT NULL,
+  `TenNV` varchar(50) NOT NULL,
+  `DienThoai` varchar(10) NOT NULL,
+  `MaTK` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phieunhap`
+--
+
+CREATE TABLE `phieunhap` (
+  `MaPN` int(11) NOT NULL,
+  `MaNV` int(11) NOT NULL,
+  `NgayLapPhieu` timestamp NOT NULL DEFAULT current_timestamp(),
+  `TongTien` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taikhoan`
+--
+
+CREATE TABLE `taikhoan` (
+  `MaTK` int(11) NOT NULL,
+  `TenTK` varchar(30) NOT NULL,
+  `MatKhau` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chitiethoadon`
+--
+ALTER TABLE `chitiethoadon`
+  ADD PRIMARY KEY (`MaMon`),
+  ADD KEY `fk_cthd_id_hd` (`MaHD`);
+
+--
+-- Indexes for table `chitietphieunhap`
+--
+ALTER TABLE `chitietphieunhap`
+  ADD PRIMARY KEY (`MaPhieuNhap`),
+  ADD KEY `fk_ctpn_id_nl` (`MaNL`);
+
+--
+-- Indexes for table `chucvu`
+--
+ALTER TABLE `chucvu`
+  ADD PRIMARY KEY (`MaChucVu`);
+
+--
+-- Indexes for table `dichvu`
+--
+ALTER TABLE `dichvu`
+  ADD PRIMARY KEY (`maDV`);
+
+--
+-- Indexes for table `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD PRIMARY KEY (`MaHD`),
+  ADD KEY `fk_hd_id_nv` (`MaNV`),
+  ADD KEY `fk_hd_id_dv` (`MaDV`);
+
+--
+-- Indexes for table `loaimon`
+--
+ALTER TABLE `loaimon`
+  ADD PRIMARY KEY (`MaLoai`);
+
+--
+-- Indexes for table `mon`
+--
+ALTER TABLE `mon`
+  ADD PRIMARY KEY (`MaMon`),
+  ADD KEY `fk_mon_id_loai` (`MaLoaiMon`);
+
+--
+-- Indexes for table `nguyenlieu`
+--
+ALTER TABLE `nguyenlieu`
+  ADD PRIMARY KEY (`MaNL`);
+
+--
+-- Indexes for table `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  ADD PRIMARY KEY (`MaNV`),
+  ADD KEY `fk_nv_id_chucvu` (`MaCV`),
+  ADD KEY `fk_nv_id_taikhoan` (`MaTK`);
+
+--
+-- Indexes for table `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  ADD PRIMARY KEY (`MaPN`),
+  ADD KEY `fk_pn_id_nv` (`MaNV`);
+
+--
+-- Indexes for table `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  ADD PRIMARY KEY (`MaTK`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chitietphieunhap`
+--
+ALTER TABLE `chitietphieunhap`
+  MODIFY `MaPhieuNhap` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `chucvu`
+--
+ALTER TABLE `chucvu`
+  MODIFY `MaChucVu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `dichvu`
+--
+ALTER TABLE `dichvu`
+  MODIFY `maDV` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hoadon`
+--
+ALTER TABLE `hoadon`
+  MODIFY `MaHD` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `loaimon`
+--
+ALTER TABLE `loaimon`
+  MODIFY `MaLoai` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mon`
+--
+ALTER TABLE `mon`
+  MODIFY `MaMon` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nguyenlieu`
+--
+ALTER TABLE `nguyenlieu`
+  MODIFY `MaNL` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  MODIFY `MaNV` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  MODIFY `MaPN` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  MODIFY `MaTK` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chitiethoadon`
+--
+ALTER TABLE `chitiethoadon`
+  ADD CONSTRAINT `fk_cthd_id_hd` FOREIGN KEY (`MaHD`) REFERENCES `hoadon` (`MaHD`),
+  ADD CONSTRAINT `fk_cthd_id_mon` FOREIGN KEY (`MaMon`) REFERENCES `mon` (`MaMon`);
+
+--
+-- Constraints for table `chitietphieunhap`
+--
+ALTER TABLE `chitietphieunhap`
+  ADD CONSTRAINT `fk_ctpn_id_nl` FOREIGN KEY (`MaNL`) REFERENCES `nguyenlieu` (`MaNL`),
+  ADD CONSTRAINT `fk_ctpn_id_pn` FOREIGN KEY (`MaPhieuNhap`) REFERENCES `phieunhap` (`MaPN`);
+
+--
+-- Constraints for table `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD CONSTRAINT `fk_hd_id_dv` FOREIGN KEY (`MaDV`) REFERENCES `dichvu` (`maDV`),
+  ADD CONSTRAINT `fk_hd_id_nv` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`);
+
+--
+-- Constraints for table `mon`
+--
+ALTER TABLE `mon`
+  ADD CONSTRAINT `fk_mon_id_loai` FOREIGN KEY (`MaLoaiMon`) REFERENCES `loaimon` (`MaLoai`);
+
+--
+-- Constraints for table `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  ADD CONSTRAINT `fk_nv_id_chucvu` FOREIGN KEY (`MaCV`) REFERENCES `chucvu` (`MaChucVu`),
+  ADD CONSTRAINT `fk_nv_id_taikhoan` FOREIGN KEY (`MaTK`) REFERENCES `taikhoan` (`MaTK`);
+
+--
+-- Constraints for table `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  ADD CONSTRAINT `fk_pn_id_nv` FOREIGN KEY (`MaNV`) REFERENCES `nhanvien` (`MaNV`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
